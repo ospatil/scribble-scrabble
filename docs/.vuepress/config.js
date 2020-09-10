ceacd78ff15ba9b69598138108ef56bb82842836
Omkar Patil <ospatil@gmail.com>
2020-09-10 12:16:10 -0400

    Switch to safe markdown-it-katex package

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  title: 'Notes',
+  description: 'Notes taken while tinkering with technologies and studying.',
   markdown: {
     // options for markdown-it-anchor
     // anchor: { permalink: false },
@@ -6,7 +8,7 @@ module.exports = {
     // toc: { includeLevel: [1, 2] },
     extendMarkdown: md => {
       // use more markdown-it plugins!
-      md.use(require('markdown-it-katex'));
+      md.use(require('@iktakahiro/markdown-it-katex'));
     }
   }
 };
